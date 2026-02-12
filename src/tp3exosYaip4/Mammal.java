03b4732ad5e31722b0e483a5be9d125a9079dbf3
Zea Baez Brandon <brandon.ZEA-BAEZ@etu.univ-amu.fr>
2026-02-12 12:18:03 +0100

    voici l'exercice 4.2

---
+++ src/tp3exosYaip4/Mammal.java
@@ -0,0 +1,10 @@
+package tp3exosYaip4;
+
+public class Mammal extends Animal{
+	public Mammal(String name) {
+		super(name);
+	}
+	public String toString() {
+		return "Mamamaml["+super.toString()+"]";
+	}
+}
---
+++ src/tp3exosYaip4/TestAnimal.java
@@ -0,0 +1,8 @@
+package tp3exosYaip4;
+
+public class TestAnimal {
+	public static void main(String[] args) {
+		Dog d1 = new Dog("brandon");
+		System.out.print(d1);
+	}
+}
